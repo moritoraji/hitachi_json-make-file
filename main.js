5052c309aede39cc1f8bc4a4f5d3af12b8223a94
moritoraji <freelance0507@gmail.com>
2026-01-29 13:59:52 +0900

    1/29 最新データ更新

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ const writeFile = util.promisify(fs.writeFile).bind(util);
 const src_file = path.resolve(path.join('xlsx', 'sheet.xlsx')); //エクセルファイル名
 const sheet_name = '2026'; //シート名
 const dest_file = path.resolve(path.join('dest', 'list.json')); //jsonファイル名
-const dest_lp_file = path.resolve(path.join('dest', 'list-lp.json')); //下層用jsonファイル名
+const dest_lp_file = path.resolve(path.join('dest', 'list-under.json')); //下層用jsonファイル名
 
 // エクセル範囲の設定
 const EXCEL_START_ROW = 2;  // データ開始行
 const EXCEL_START_ROW_INDEX = EXCEL_START_ROW - 1;
-const EXCEL_END_ROW = 36;   // データ終了行
+const EXCEL_END_ROW = 38;   // データ終了行
 const EXCEL_RANGE = `A${EXCEL_START_ROW}:M${EXCEL_END_ROW}`;  // エクセル範囲
 
 const readXLSX = () => {
@@ -44,14 +44,14 @@ const readXLSX = () => {
       const r_id = getCellValue(0, r);
       const r_name = getCellValue(1, r);
       const r_des = getCellValue(2, r);
-      const r_pride = getCellValue(3, r);
-      const r_link2 = getCellValue(4, r);
-      const r_link3 = getCellValue(5, r);
-      const r_link1 = getCellValue(10, r);
-      const r_link4 = getCellValue(11, r);
-      const r_photo = getCellValue(12, r);
-      const r_entry = getCellValue(13, r);
-      const r_category = getCellValue(14, r);
+      const r_link2 = getCellValue(3, r);
+      const r_link3 = getCellValue(4, r);
+      const r_link1 = getCellValue(9, r);
+      const r_link4_raw = getCellValue(10, r);
+      const r_link4 = r_link4_raw === '-' ? 'none' : r_link4_raw;
+      const r_photo = getCellValue(11, r);
+      const r_entry = getCellValue(12, r);
+      const r_category = getCellValue(13, r);
 
       // リスト形式のデータを取得
       const getListValue = (col) => {
@@ -60,16 +60,15 @@ const readXLSX = () => {
         return ["すべて", ...cell.v.toString().trim().split('\r\n')];
       };
 
-      const r_syokusyus = getListValue(6);
-      const r_gakubus = getListValue(7);
-      const r_bunyas = getListValue(8);
-      const r_kodawaris = getListValue(9);
+      const r_syokusyus = getListValue(5);
+      const r_gakubus = getListValue(6);
+      const r_bunyas = getListValue(7);
+      const r_kodawaris = getListValue(8);
 
       ret.push({
         id: r_id,
         name: r_name,
         des: r_des,
-        pride: r_pride,
         photo: r_photo,
         syokusyus: r_syokusyus,
         gakubus: r_gakubus,
@@ -143,9 +142,9 @@ const createLPJSON = async (data) => {
 
     const json = JSON.stringify(lpData, null, '\t');
     await writeFile(dest_lp_file, json);
-    console.log('下層用JSONファイル（list-lp.json）の作成に成功しました。');
+    console.log('下層用JSONファイル（list-under.json）の作成に成功しました。');
   } catch (error) {
-    console.error('下層用JSONファイル（list-lp.json）の作成に失敗しました:', error);
+    console.error('下層用JSONファイル（list-under.json）の作成に失敗しました:', error);
     throw error;
   }
 };
